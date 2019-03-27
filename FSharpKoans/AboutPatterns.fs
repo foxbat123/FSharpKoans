8b8de2ac8e4499ec85cd7ae577a43290e0a9646c
Ryan McDonald <ryandennismcdonald@gmail.com>
2019-03-27 08:11:12 +0200

    27 March

--- FSharpKoans/AboutPatterns.fs
+++ FSharpKoans/AboutPatterns.fs
@@ -22,9 +22,9 @@ module ``09: Advanced pattern-matching`` =
             | "wut" | "lol" -> "yolo"
             | "sunrise"
             | "sunset" -> "transition"
-            | FILL__ME_IN
-            | FILL__ME_IN
-            | FILL__ME_IN -> "failure"
+            | "Johnny Walker"
+            | "Bell's"
+            | "vodka" -> "failure"
             | _ -> "lolwut"
         f "lol" |> should equal "yolo"
         f "wut" |> should equal "yolo"
@@ -37,7 +37,7 @@ module ``09: Advanced pattern-matching`` =
         let f input =
             match input with
             | 0,0 -> "Both 0"
-            | ___ | ___ -> sprintf "One 0, one %d" __
+            | 3,0 | 0,4 -> sprintf "One 0, one %d" (_,x) = input
             | _ -> "No 0"
         f (3,0) |> should equal "One 0, one 3"
         f (0, 4) |> should equal "One 0, one 4"
